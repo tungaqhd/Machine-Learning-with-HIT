f751dafbcee579ab197cc3cacf0c7876d1eadcae
Nguyễn Sỹ Tùng <tungaqhd@gmail.com>
2019-05-25 18:57:01 +0700

    Creat b2

---
+++ Week 1/b2.py
@@ -0,0 +1,13 @@
+import json
+def get_name(path):
+    arr = path.strip().split('/')
+    return arr[-1]
+file = open('data/path.txt')
+wr = open('data/path.json', 'w')
+add = list()
+ch = dict()
+for path in file:
+    ch['path'] = path.replace('\n', '')
+    ch['file_name'] = get_name(path)
+    add.append(ch.copy())
+json.dump(add, wr)
