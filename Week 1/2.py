c6ca3f03c55a8a596421f059efb31e6ad9164c2d
Nguyễn Sỹ Tùng <tungaqhd@gmail.com>
2019-05-25 20:15:13 +0700

    Update 2.py

--- Week 1/2.py
+++ Week 1/2.py
@@ -7,7 +7,7 @@ wr = open('data/path.json', 'w')
 add = list()
 ch = dict()
 for path in file:
-    ch['path'] = path.replace('\n', '')
+    ch['path'] = path.strip().replace('\n', '')
     ch['file_name'] = get_name(path)
     add.append(ch.copy())
 json.dump(add, wr)
