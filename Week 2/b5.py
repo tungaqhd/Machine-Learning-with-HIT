f1697d849ba9ef7b0c92f5d1cd9cf95168eba3eb
Nguyễn Sỹ Tùng <tungaqhd@gmail.com>
2019-05-30 01:05:51 +0700

    Update b5.py
    Add Exception Handling for input

--- Week 2/b5.py
+++ Week 2/b5.py
@@ -1,4 +1,8 @@
 def pal(s):
-    return s == s[::-1]
+    try:
+        check = s.islower()
+        return s == s[::-1]
+    except:
+        return TypeError('S must be a string')
 s = input()
 print(pal(s))
